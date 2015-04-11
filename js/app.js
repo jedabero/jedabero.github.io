efeed4b911224384eee07846ae5e5d42e728f043
jedabero <jedabero@hotmail.com>
2015-04-11 11:35:44 -0500

    Moindex actualizado

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 (function () {
 	var app = angular.module("jedabero", []);
 
-	
+	app.controller('testController', function($scope) {
+		$scope.testvar = ["Hola", "Hello", "Hi", "Ohayo", "GG"];
+		$scope.rand = Math.floor( $scope.testvar.length * Math.random() );
+	});
+
+	app.controller('objController', function($scope) {
+		$scope.firstname = "Juan",
+		$scope.lastname = "Echeverria";
+	});
 
 })();
 
