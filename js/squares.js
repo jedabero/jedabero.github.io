5112153b4816f56dda134f7c507c24bc5e869b51
jedabero <jedabero20@gmail.com>
2018-05-09 16:38:22 -0500

    Updated random

--- js/squares.js
+++ js/squares.js
@@ -9,6 +9,10 @@
         $scope.firstname = "Juan",
         $scope.lastname = "Echeverria";
     });
+
+    app.controller('footerController', ['$scope', function (scope) {
+		scope.year = (new Date()).getFullYear();
+	}]);
 })();
 
 window.countFPS = (function () {
@@ -37,16 +41,16 @@ debugSize = $("#debugSize");
 debugFPS = $("#debugFPS");
 var ctx = c.getContext("2d");
 
-w = 500;
-h = 250;
+var w = 500;
+var h = 250;
 
 debug = false;
 areNotBlank = false;
 
 draw();
 
-setInterval(draw, 82);
-//setInterval(draw, 1333);
+setInterval(draw, 250);
+// setInterval(draw, 1333);
 
 function randRGB () {
     R = Math.floor(Math.random()*255);
@@ -56,21 +60,32 @@ function randRGB () {
 }
 
 function draw(){
-    if (Math.floor(Math.random()*100)<23) {
+    if (Math.floor(Math.random()*100)<16) {
         ctx.clearRect(0, 0, w, h);
     }
     ctx.strokeStyle="rgb("+randRGB().join(", ")+")";
-    wi = Math.floor(Math.random()*w);
-    he = Math.floor(Math.random()*h);
-    x = Math.floor(Math.random()*(w-wi));
-    y = Math.floor(Math.random()*(h-he));
-    ctx.strokeRect(x, y, wi, he);
+    if (Math.floor(Math.random()*100)<50) { // Draw a rectangle
+        var wi = Math.floor(Math.random()*w);
+        var he = Math.floor(Math.random()*h);
+        var x = Math.floor(Math.random()*(w-wi));
+        var y = Math.floor(Math.random()*(h-he));
+        ctx.strokeRect(x, y, wi, he);
+    } else { // Draw a circle
+        var d = Math.floor(Math.random()*h);
+        var r = d / 2;
+        var x = r + Math.floor(Math.random()*(w-d));
+        var y = r + Math.floor(Math.random()*(h-d));
+        ctx.beginPath();
+        ctx.arc(x, y, r, 0, 2 * Math.PI);
+        ctx.stroke();
+    }
+    
     
     if(debug) {
-        //Show position and size
-        debugPos.text("pos: ("+x+","+y+")");
-        debugSize.text("size: ("+wi+","+he+")");
-        debugFPS.text("FPS: "+countFPS());
+        // Show position and size
+        // debugPos.text("pos: ("+x+","+y+")");
+        // debugSize.text("size: ("+wi+","+he+")");
+        debugFPS.text("FPS: "+window.countFPS());
         if (!areNotBlank) {
             areNotBlank = true;
         }
