200864e36679a3aaac8969e84c65f613dfa3472d
Jeison Berdugo <jedabero20@gmail.com>
2025-11-30 19:34:28 -0500

    add pages workflow

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const basePath = process.env.NEXT_BASE_PATH || '';
+const assetPrefix = process.env.NEXT_ASSET_PREFIX || undefined;
+
 const nextConfig = {
   output: 'export',
+  basePath,
+  assetPrefix,
   images: {
     unoptimized: true
   }
